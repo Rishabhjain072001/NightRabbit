c9a0c19825f67bd5d87b5dfc51f7de4adc002144
Rishabhjain072001 <rishabhjain072001@gmail.com>
2025-01-01 17:23:07 +0530

    Added elastic search for docker

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,8 +1,5 @@
 Searchkick.client = Elasticsearch::Client.new(
   host: ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200',
-  transport_options: {
-    headers: {
-      'Authorization' => "Basic #{Base64.encode64("#{ENV['ELASTICSEARCH_USERNAME']}:#{ENV['ELASTICSEARCH_PASSWORD']}")}"
-    }
-  }
+  retry_on_failure: true,
+  transport_options: { request: { timeout: 5 } }
 )
