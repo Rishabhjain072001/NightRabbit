3152efc2056c0d13c073a4a9c1095b4426311db0
Rishabhjain072001 <rishabhjain072001@gmail.com>
2025-01-03 21:55:09 +0530

    fix

--- config/application.rb
+++ config/application.rb
@@ -35,8 +35,6 @@ module NightRabbit
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.middleware.delete ActionDispatch::Cookies
-    config.middleware.delete ActionDispatch::Session::CookieStore
-    config.api_only = true
+    config.api_only = false
   end
 end
