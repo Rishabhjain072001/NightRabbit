14b4228a789a67c3b466a3c286bc92be59453820
Rishabhjain072001 <rishabhjain072001@gmail.com>
2025-01-11 16:32:30 +0530

    Fix

--- app/jobs/reindex_job.rb
+++ app/jobs/reindex_job.rb
@@ -1,17 +1,11 @@
 class ReindexJob < ApplicationJob
   queue_as :default
 
-  def perform(model_name, start_after_id = nil, last_id = nil)
+  def perform(model_name)
     model = model_name.constantize
     index = 0
 
-    # Define the base scope
-    scope = model.where("id > ?", start_after_id || 0)
-    
-    # Apply the condition for the last ID if provided
-    scope = scope.where("id <= ?", last_id) if last_id.present?
-
-    scope.find_in_batches(batch_size: 1000) do |batch|
+    model.find_in_batches(batch_size: 5000) do |batch|
       index += 1
       Rails.logger.info "Reindexing batch #{index}, batch size: #{batch.count}"
 
