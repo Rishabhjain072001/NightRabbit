409a219bbce36533e5de852c8f349a37fc6942c0
Rishabhjain072001 <rishabhjain072001@gmail.com>
2025-01-13 20:49:08 +0530

    Updated worker

--- app/jobs/reindex_job.rb
+++ app/jobs/reindex_job.rb
@@ -1,11 +1,11 @@
 class ReindexJob < ApplicationJob
   queue_as :default
 
-  def perform(model_name)
+  def perform(model_name, start_id = 1)
     model = model_name.constantize
     index = 0
 
-    model.find_in_batches(batch_size: 5000) do |batch|
+    model.where("id >= ?", start_id).find_in_batches(batch_size: 5000) do |batch|
       index += 1
       Rails.logger.info "Reindexing batch #{index}, batch size: #{batch.count}"
 
