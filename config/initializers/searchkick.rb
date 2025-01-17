6b8f074ae4e9ac82451f654cef4245ea49754dcd
Rishabhjain072001 <rishabhjain072001@gmail.com>
2025-01-17 21:46:11 +0530

    Added elsatic search creds

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,5 +1,6 @@
 Searchkick.client = Elasticsearch::Client.new(
   host: ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200',
-  retry_on_failure: true,
-  transport_options: { request: { timeout: 5 } }
+  transport_options: { request: { timeout: 5 } },
+  user: ENV['ELASTICSEARCH_USERNAME'],        # Replace with your Elasticsearch username
+  password: ENV['ELASTICSEARCH_PASSWORD']     # Replace with your Elasticsearch password
 )
